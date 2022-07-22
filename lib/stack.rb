590ce5d23aad82231b21cd1d08bff86b8c97db40
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-21 21:33:50 -0700

    commit tree a3581cf25049c700c04f7cc231cbdb94c77a906d

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,49 @@
 # your code here
+
+class Stack
+
+    def initialize(limit = 10)
+        @stack = []
+        if limit
+            @limit = limit
+        end
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.size == 0    
+    end
+
+    def full?
+        @stack.size == @limit
+    end
+
+    def push(value)
+        if @stack.size < @limit
+            @stack.push(value)
+        else
+            raise "Stack Overflow"
+        end 
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def search(value)
+        if @stack.include? value
+            @stack.length - (@stack.index value) - 1
+        else
+            -1
+        end
+    end
+
+    def peek
+        length = @stack.length
+        @stack[length-1]
+    end
+
+end
